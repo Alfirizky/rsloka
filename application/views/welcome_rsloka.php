0d773172cec8472519aa9dedb8d56e2f74ea663a
ichlasul0899 <ichlasul0899@gmail.com>
2020-04-17 14:35:35 +0700

    Menambahkan Halaman awal rsloka

---
+++ application/views/welcome_rsloka.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RS Loka</title>
+</head>
+<body>
+    <center>
+        <h3>Welcom To RS Loka</h3>
+    </center>
+    
+</body>
+</html></html>
