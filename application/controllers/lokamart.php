7ba2283a52c08d2e1fa2a15b0b98adc600a6227c
Alfi Rizky <mochalfireynald@gmail.com>
2020-04-25 17:10:16 +0700

    Membuat lokamart

---
+++ application/controllers/lokamart.php
@@ -0,0 +1,174 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>LokaMart</title>
+		<script src="assets/js/jquery.min.js"></script>
+		<link rel="stylesheet" href="assets/css/bootstrap.min.css" />
+		<script src="assets/js/bootstrap.min.js"></script>
+		<style>
+		.popover
+		{
+		    width: 100%;
+		    max-width: 800px;
+		}
+		</style>
+	</head>
+	<body>
+		<div class="container">
+			<br />
+			<h3 align="center">LokaMart</h3>
+			<p> <a href="#">kembali</a> </p>
+			<br />
+			<nav class="navbar navbar-light bg-light" role="navigation">
+				<div class="container-fluid">
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+						<span class="sr-only">Menu</span>
+						<span class="glyphicon glyphicon-menu-hamburger"></span>
+						</button>
+						<a class="navbar-brand" href="/">Total</a>
+					</div>
+
+					<div id="navbar-cart" class="navbar-collapse collapse">
+						<ul class="nav navbar-nav">
+							<li>
+								<a id="cart-popover" class="btn" data-placement="bottom" title="Shopping Cart">
+									<span class="glyphicon glyphicon-shopping-cart"></span>
+									<span class="badge"></span>
+									<span class="total_price">Rp 0</span>
+								</a>
+							</li>
+						</ul>
+					</div>
+
+				</div>
+			</nav>
+			<div id="popover_content_wrapper" style="display: none">
+				<span id="cart_details"></span>
+				<div align="right">
+					<a href="#" class="btn btn-primary" id="check_out_cart">
+					<span class="glyphicon glyphicon-shopping-cart"></span> Bayar
+					</a>
+					<a href="#" class="btn btn-default" id="clear_cart">
+					<span class="glyphicon glyphicon-trash"></span> Hapus semua
+					</a>
+				</div>
+			</div>
+
+			<div id="display_item">
+
+
+			</div>
+
+		</div>
+	</body>
+</html>
+
+<script>
+$(document).ready(function(){
+
+	load_product();
+
+	load_cart_data();
+
+	function load_product()
+	{
+		$.ajax({
+			url:"fetch_item.php",
+			method:"POST",
+			success:function(data)
+			{
+				$('#display_item').html(data);
+			}
+		});
+	}
+
+	function load_cart_data()
+	{
+		$.ajax({
+			url:"fetch_cart.php",
+			method:"POST",
+			dataType:"json",
+			success:function(data)
+			{
+				$('#cart_details').html(data.cart_details);
+				$('.total_price').text(data.total_price);
+				$('.badge').text(data.total_item);
+			}
+		});
+	}
+
+	$('#cart-popover').popover({
+		html : true,
+        container: 'body',
+        content:function(){
+        	return $('#popover_content_wrapper').html();
+        }
+	});
+
+	$(document).on('click', '.add_to_cart', function(){
+		var product_id = $(this).attr("id");
+		var product_name = $('#name'+product_id+'').val();
+		var product_price = $('#price'+product_id+'').val();
+		var product_quantity = $('#quantity'+product_id).val();
+		var action = "add";
+		if(product_quantity > 0)
+		{
+			$.ajax({
+				url:"action.php",
+				method:"POST",
+				data:{product_id:product_id, product_name:product_name, product_price:product_price, product_quantity:product_quantity, action:action},
+				success:function(data)
+				{
+					load_cart_data();
+					alert("item berhasil ditambahkan ke keranjang");
+				}
+			});
+		}
+		else
+		{
+			alert("GUNAKAN ANGKA | ISI MINIMAL 1");
+		}
+	});
+
+	$(document).on('click', '.delete', function(){
+		var product_id = $(this).attr("id");
+		var action = 'remove';
+		if(confirm("Apakah anda ingin menghapus item?"))
+		{
+			$.ajax({
+				url:"action.php",
+				method:"POST",
+				data:{product_id:product_id, action:action},
+				success:function()
+				{
+					load_cart_data();
+					$('#cart-popover').popover('hide');
+					alert("Berhasil menghapus item");
+				}
+			})
+		}
+		else
+		{
+			return false;
+		}
+	});
+
+	$(document).on('click', '#clear_cart', function(){
+		var action = 'empty';
+		$.ajax({
+			url:"action.php",
+			method:"POST",
+			data:{action:action},
+			success:function()
+			{
+				load_cart_data();
+				$('#cart-popover').popover('hide');
+				alert("Keranjang anda berhasil dikosongkan");
+			}
+		});
+	});
+
+});
+
+</script>
